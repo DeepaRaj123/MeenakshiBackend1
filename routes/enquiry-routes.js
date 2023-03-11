4a86f66faf85d72e4fa3b587839ac3606b304f8e
DeepaRaj123 <36413136+DeepaRaj123@users.noreply.github.com>
2023-03-11 14:08:12 +0530

    Update enquiry-routes.js

--- routes/enquiry-routes.js
+++ routes/enquiry-routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getEnquiries,postEnquiries,patchEnquiries} = require('../controllers/enquiryController');
+const {getEnquiries,postEnquiries,patchEnquiries,showwelcomeMessage} = require('../controllers/enquiryController');
 const AuthMiddleware = require('../middleware/authMiddleware');
 
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/getEnquiries', getEnquiries);
 router.post('/postEnquiries', postEnquiries);
 router.patch('/patchEnquiries/:id', patchEnquiries);
+router.get('/', showwelcomeMessage);
 
 module.exports = {
     routes: router
